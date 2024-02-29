8545dafda410987e6e99cbed4d21c8efca8607ee
10Sumi10 <sushma.gurung478@gmail.com>
2024-02-29 08:19:06 +0000

    Added forcast

--- src/index.js
+++ src/index.js
@@ -51,3 +51,16 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("London");
+
+let forecast = document.querySelector("#forecast");
+
+forecast.innerHTML = `<div class="weather-forecast-day">
+<div class="weather-forecast-date">Tue</div>
+<div class="weather-forecast-icon">🌤️</div>
+<div class="weather-forecast-temperatures">
+<div class="weather-forecast-temperature">
+<strong>15°</strong>
+</div>
+<div class="weather-forecast-temperature">9°</div>
+</div>
+</div>`;
